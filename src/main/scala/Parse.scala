c87b78ab2a5218cc1b5a73c600af6916cfda66a5
watanabe <nyoronyoro.watanabe3@gmail.com>
2013-12-10 23:58:47 +0900

    HighChartを使ったチャートを生成出来るようにしてみた。

--- src/main/scala/Parse.scala
+++ src/main/scala/Parse.scala
@@ -4,44 +4,7 @@ import scala.Array.canBuildFrom
 object Parse extends Logging {
   type IndexedString = Tuple2[String, Int]
 
-  trait CostBase {
-    def s: Double
-    def e: Double
-    def row: Int
-    protected def additional: List[String]
-    def elapse() = e - s
-    def csv() = List(s.toString, e.toString, elapse, row.toString) ++ additional
-    def excel(sum: CostBase): List[String] = {
-      List(s.toString, e.toString, elapse.toString, (elapse / sum.e * 100).toString, row.toString) ++ additional
-    }
-  }
-
-  case class Cost(s: Double, e: Double, row: Int, width: Int) extends CostBase {
-    protected def additional() = {
-      List(width.toString)
-    }
-  }
-
-  case class ActualTime(s: Double, e: Double, row: Int, loop: Int) extends CostBase {
-    protected def additional() = {
-      List(loop.toString)
-    }
-  }
-
-  case class Plan(line: Int, op: String, cost: Cost, actualTime: ActualTime, desc: List[String]) {
-    def csv(sum: Plan) = {
-      op +: List(cost.csv, actualTime.csv, desc).flatten
-    }
-    def excelHeader() = {
-      List("No", "Op", "CS", "CE", "elapse", "CRatio", "CRow", "Width", "AS", "AE", "elapse", "ARatio", "ARow", "Loop").mkString("\t")
-    }
-    def excel(sum: Plan): List[String] = {
-      val dataList = List(cost.excel(sum.cost), actualTime.excel(sum.actualTime)).flatten
-      (line + 1).toString +: op +: dataList :+ desc.mkString("\"", "\n", "\"")
-    }
-  }
-
-  object RE {
+  private object RE {
     val FP = """[0-9]+\.[0-9]+"""
     val INT = "[0-9]+"
     val FPRANGE = s"($FP)\\.\\.($FP)"
@@ -107,4 +70,7 @@ object Parse extends Logging {
     }
   }
 
+  def getPlans() = {
+    
+  }
 }
---
+++ src/main/scala/Plan.scala
@@ -0,0 +1,36 @@
+case class Plan(line: Int, op: String, cost: Cost, actualTime: ActualTime, desc: List[String]) {
+  def csv(sum: Plan) = {
+    op +: List(cost.csv, actualTime.csv, desc).flatten
+  }
+  def excelHeader() = {
+    List("No", "Op", "CS", "CE", "elapse", "CRatio", "CRow", "Width", "AS", "AE", "elapse", "ARatio", "ARow", "Loop").mkString("\t")
+  }
+  def excel(sum: Plan): List[String] = {
+    val dataList = List(cost.excel(sum.cost), actualTime.excel(sum.actualTime)).flatten
+    (line + 1).toString +: op +: dataList :+ desc.mkString("\"", "\n", "\"")
+  }
+}
+
+case class Cost(start: Double, end: Double, row: Int, width: Int) extends CostBase {
+  protected def additional() = {
+    List(width.toString)
+  }
+}
+
+case class ActualTime(start: Double, end: Double, row: Int, loop: Int) extends CostBase {
+  protected def additional() = {
+    List(loop.toString)
+  }
+}
+
+trait CostBase {
+  def start: Double
+  def end: Double
+  def row: Int
+  protected def additional: List[String]
+  def elapse() = end - start
+  def csv() = List(start.toString, end.toString, elapse, row.toString) ++ additional
+  def excel(sum: CostBase): List[String] = {
+    List(start.toString, end.toString, elapse.toString, (elapse / sum.end * 100).toString, row.toString) ++ additional
+  }
+}
